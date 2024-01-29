96380d0e512fc0aef9e437ce097748d860258001
Erode <orphan0@hotmail.com>
2024-01-29 14:51:20 +0100

    add first variable

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sélectionner le formulaire
+let form = document.querySelector("form");
+
+// Sélectionner la liste des commentaires
+let commentList = document.getElementById("comment-list");
+
+// Sélectionner le message d'erreur
+let errorMessage = document.getElementById("error-message");
